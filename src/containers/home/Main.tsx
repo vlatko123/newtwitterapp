94e38276c40adbe3ce1f96c0a8c914d3fc4cf52a
vlatko123 <v.bogojevski@yahoo.com>
2022-03-04 22:49:58 +0100

    Added changes form navbar and component connected with navbar

--- src/containers/home/Main.tsx
+++ src/containers/home/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Main = () => {
     return(
-        <main className='col-6'style ={{backgroundColor:'yellow', height: '100vh'}}>
+        <main className='col-6'style ={{height: '100vh', border: "1px solid white"}}>
               
         </main>
     )
