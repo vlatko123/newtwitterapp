8e56f0e41b0b37873e7a1b5c35d2d729a2f8441a
vlatko123 <v.bogojevski@yahoo.com>
2022-03-05 03:21:57 +0100

    Created components in aside part, created initial design

---
+++ src/components copy/search/SearchInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+export const SearchInput = () => {
+    return(
+        <Styled.Container>
+            <Styled.Input type = "text" placeholder='Search Twitter'/>
+        </Styled.Container>
+    )
+}
+
+const Styled = {
+    Container: styled.div`
+    width: 100%;
+    margin: 10px; 
+    `,
+    Input: styled.input`
+    padding: 10px;
+    width: 100%;
+    border-radius: 20px;
+    background-color: rgb(32, 35, 39);
+    border: none;
+    `
+}
+    
--- src/containers/home/Search.tsx
+++ src/containers/home/Search.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { SearchInput } from '../../components copy/search/SearchInput';
+import { Trending } from '../../components copy/trending/Trending';
 
 
 export const Search = () => {
     return(
-        <aside className='col-3'style ={{height: '100vh'}}>
-           
-            </aside>
+        <Styled.Container className='col-3'style ={{height: '100vh'}}>
+            <SearchInput />
+            <Trending />
+            </Styled.Container>
     )
+}
+
+const Styled = {
+    Container: styled.aside`
+    margin: 0 auto;
+
+    `
 }
